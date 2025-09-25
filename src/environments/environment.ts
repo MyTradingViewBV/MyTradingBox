e93d1944a5c99ef85e3dcadabfbeddfe455f9d4a
Erwin Torrenga <erwin@ah-automation.nl>
2025-09-25 20:34:36 +0200

    ..

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment: IEnvironment = {
   production: false,
   version: '#{Build.BuildNumber}#',
   //apiUrl: 'https://localhost:7212/',
-  apiUrl: '//https://bot002api-gbh3hwe2egepfph6.swedencentral-01.azurewebsites.net/'
+  apiUrl: 'https://bot002api-gbh3hwe2egepfph6.swedencentral-01.azurewebsites.net/'
 };
