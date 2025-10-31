2d75cf6d4bd82a46344466dfaec5b040529aaf64
Erwin <erwin.torrenga@live.nl>
2025-10-31 10:46:34 +0100

    added unit test

---
+++ src/app/components/chart/chart-component.spec.ts
@@ -0,0 +1,23 @@
+// Adjusted spec to test existing ChartComponent instead of missing ChartTestComponent
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ChartComponent } from './chart-component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+describe('ChartComponent', () => {
+  let component: ChartComponent;
+  let fixture: ComponentFixture<ChartComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ChartComponent, HttpClientTestingModule],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
