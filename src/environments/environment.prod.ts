ef2cfadaa08c83a03b17bef0f27bab181b6037b8
Erwin <erwin.torrenga@live.nl>
2025-11-30 18:24:09 +0100

    nadroid notification

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 import { IEnvironment } from './ienvironment';
 
 export const environment: IEnvironment = {
-  production: true,
+  production: false,
   version: '#{Build.BuildNumber}#',
   apiUrl: 'https://bot002api-gbh3hwe2egepfph6.swedencentral-01.azurewebsites.net/'
   //apiUrl: 'https://localhost:7212/',
